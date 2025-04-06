b615458b4d71934416bf96335281c6430c7bcf27
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2025-04-06 13:44:28 +0200

    Setup React frontend for user-app

---
+++ user-app/client/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
