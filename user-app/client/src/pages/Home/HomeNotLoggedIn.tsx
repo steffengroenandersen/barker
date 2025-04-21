5904ac142b9cb1925d31cc69d7f7ccadf9c6f9b3
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2025-04-21 13:30:10 +0200

    Add Input and Button, add login form

--- user-app/client/src/pages/Home/HomeNotLoggedIn.tsx
+++ user-app/client/src/pages/Home/HomeNotLoggedIn.tsx
@@ -1,9 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { useState } from "react";
+
 export default function HomeNotLoggedIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
   return (
     <>
       <section className="flex justify-center items-center h-screen">
         <div className="bg-white p-5 rounded-md shadow w-[400px] sm:w-[800px] m-2">
           <h1>Login</h1>
+          <form className="flex flex-col gap-2">
+            <Input
+              type="email"
+              placeholder="Email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Button onClick={login}>Login</Button>
+          </form>
         </div>
       </section>
     </>
