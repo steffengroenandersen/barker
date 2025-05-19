1740deefb24bcbfb819e8d908a10f655903f525e
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2025-05-19 11:17:55 +0200

    Remove

--- user-app/client/src/pages/Home/HomeLoggedIn.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomeLoggedIn() {
-  return (
-    <>
-      <h1>HomeLoggedIn</h1>
-    </>
-  );
-}
--- user-app/server/routes/api/api.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import userRoutes from "./user/user.js";
-
-const router = express.Router();
-
-router.use("/user", userRoutes);
-
-export default router;
